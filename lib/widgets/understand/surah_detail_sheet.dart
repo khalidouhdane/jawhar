4f5c8b3c5fe04c907be9383fa154e9ebb5164f6e
Khalid Ouhdane <khalid.ouhdane13@gmail.com>
2026-05-18 21:43:14 +0100

    fix(l10n): resolve lucide_icons import errors

--- lib/widgets/understand/surah_detail_sheet.dart
+++ lib/widgets/understand/surah_detail_sheet.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quran_app/data/surah_metadata.dart';
